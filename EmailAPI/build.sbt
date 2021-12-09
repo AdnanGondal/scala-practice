239c46e1d322777eb78359a596b3c60d2555534b
Adnan Gondal <adnangondal1@hotmail.com>
2021-12-09 12:52:25 +0000

    setup API boilerplate

--- EmailAPI/build.sbt
+++ EmailAPI/build.sbt
@@ -6,3 +6,10 @@ scalaVersion := "2.13.7"
 
 libraryDependencies += "com.github.daddykotex" %% "courier" % "3.0.1"
 
+val AkkaVersion = "2.6.8"
+val AkkaHttpVersion = "10.2.7"
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion
+)
--- EmailAPI/src/main/scala/emailapi/Server.scala
+++ EmailAPI/src/main/scala/emailapi/Server.scala
@@ -4,27 +4,53 @@ import akka.http.javadsl.ServerBinding
 
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import scala.io.StdIn
 
 object Server extends App {
 
   //Write your code below here
+  implicit val system = ActorSystem(Behaviors.empty, "my-system")
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.executionContext
+  val port = 8080
+  val host = "localhost"
 
-
+  val route =
+    path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      } ~ post {
+        entity(as[String]) { str =>
+          complete(s"Received String : ${str}")
+        }
+      }
+    }
   // Write your code above here
 
   // Note: You have to bind your requestHandler or routes here
-  val binding: Future[ServerBinding]
+  val bindingFuture = Http().newServerAt("localhost", port).bind(route)
   // binding = Http().newServerAt("localhost", 8080).bindSync(requestHandler)
   // or
   // binding = Http().newServerAt("localhost", 8080).bind(route)
 
-  binding.onComplete {
+  bindingFuture.onComplete {
     case Success(value) =>
       println(s"Server is listening on http://$host:${port}")
     case Failure(exception) =>
       println(s"Failure : $exception")
       system.terminate()
   }
+
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+
 
 
 }

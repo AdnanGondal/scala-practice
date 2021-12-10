ffa28856c9d4df8f14168cb67512c349146f3ec4
Adnan Gondal <adnangondal1@hotmail.com>
2021-12-10 11:10:29 +0000

    api done, need to make it work

--- EmailAPI/src/main/scala/emailapi/Server.scala
+++ EmailAPI/src/main/scala/emailapi/Server.scala
@@ -9,6 +9,8 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+
 import scala.io.StdIn
 
 object Server extends App {
@@ -20,7 +22,7 @@ object Server extends App {
   val port = 8080
   val host = "localhost"
 
-  val route =
+  val helloRoute =
     path("hello") {
       get {
         complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
@@ -30,6 +32,86 @@ object Server extends App {
         }
       }
     }
+
+
+  val mailingListRoute = pathEndOrSingleSlash {
+    concat(
+      get{
+        complete("getting all mailing lists")
+      },
+      post{
+        entity(as[String]) { str => {
+          complete(s"Posting a new mailing list: "+ str)
+        }
+        }
+      }
+    )
+  }
+
+  // localhost/lists/
+//  GET: get all mailing lists
+  // POST: make a new mailing list
+
+// localhost/lists/{ID}
+// DELETE: delete meailing list
+  // POST: add email to mailing list // or multiple emails
+  // GET: get all emails in the mailing list
+
+
+// localhost/lists/{name}/email
+  // POST: SEND EMAILS
+
+  // localhost/lists/{name}/email/{ID}
+  // DELETE: remove email from mailing list
+
+  val changeMailingListRoute =  path(Segment) { name =>
+    concat(
+      get {
+        // check rejectEmptyResponse
+        complete(s"get the following mailing list: ${name}")
+      },
+      post {
+        entity(as[String]){str => {
+          complete(s"adding to the mailing list  ${name} with "+str)
+        }
+        }
+      },
+      delete {
+        // check out onSuccess
+        complete(s"delete the mailing list: ${name}")
+      }
+    )
+  }
+
+  val listsEmailRoute: Route =
+    pathPrefix( Segment / "email"){ name =>
+      concat(
+        pathEndOrSingleSlash{
+          post{
+            complete(s"${name} will be emailed")
+          }
+        }
+       ,
+        path(Segment) { email=>{
+          concat(
+            delete{
+              complete(s"deleting email ${email} from list ${name}")
+            }
+          )
+        }
+
+        }
+      )
+    }
+
+  val listsRoute: Route =
+    pathPrefix("lists") {
+      concat( mailingListRoute
+        , changeMailingListRoute,listsEmailRoute
+      )
+    }
+
+  val route = helloRoute ~ listsRoute
   // Write your code above here
 
   // Note: You have to bind your requestHandler or routes here
@@ -50,7 +132,5 @@ object Server extends App {
   bindingFuture
     .flatMap(_.unbind()) // trigger unbinding from the port
     .onComplete(_ => system.terminate()) // and shutdown when done
-
-
 
 }
--- EmailAPI/src/main/scala/emailapi/models/MailingList.scala
+++ EmailAPI/src/main/scala/emailapi/models/MailingList.scala
@@ -1,7 +1,13 @@
 package emailapi.models
 
+// use case class
+// with companion object.
+// where would addEmailAccount go?
+// what else goes into a companion object
+// what is the best Collection to use to store emails.
 
-class MailingList(name: String, emails: Array[EmailAccount]) {
+
+case class MailingList(name: String, emails: Array[EmailAccount]) {
 
   def addEmailAccount(email: String, name: String) {
     emails :+ EmailAccount(name, email)
